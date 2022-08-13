6f6ce8af1d5f2d96f7836064abb867549a372871
aditya <adityakumar222333@gmail.com>
2022-08-13 22:28:25 +0530

    🚀 Deploy:  firebase init and deploy script added

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth"
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDH4d_qvkqvKkEkUSfxNvharNXPWUvVQu8",
-  authDomain: "task-manager-8f32d.firebaseapp.com",
-  projectId: "task-manager-8f32d",
-  storageBucket: "task-manager-8f32d.appspot.com",
-  messagingSenderId: "313217767098",
-  appId: "1:313217767098:web:3cf84a3fa7f00aa9de5483",
+  apiKey: "AIzaSyA9_CqmujTBj-al-f4TtwnEOAlIgNhUMKw",
+
+  authDomain: "task-x-prod.firebaseapp.com",
+
+  projectId: "task-x-prod",
+
+  storageBucket: "task-x-prod.appspot.com",
+
+  messagingSenderId: "750890250328",
+
+  appId: "1:750890250328:web:7d95e3e2afa669cf4f1a2d",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
+export const auth = getAuth(app);
