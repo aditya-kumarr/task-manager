aa9d5de0cc90bd58cff193fc07733205c1e7cfe2
aditya <adityakumar222333@gmail.com>
2022-08-19 15:03:23 +0530

    🚧 Work in Progress:adding an icon to task

--- src/containers/TaskInQueue.jsx
+++ src/containers/TaskInQueue.jsx
@@ -1,25 +1,37 @@
 import React from "react";
 import styled from "styled-components";
+import { FaThinkPeaks, FaBrain, FaBook, FaCube, FaBurn } from "react-icons/fa";
 import { motion } from "framer-motion";
 
-export function TaskInQueue(props) {
+const IconMap = {
+  money: <FaThinkPeaks />,
+};
+
+export function TaskInQueue({ index, item, setSelectedTask, onTaskClick }) {
   return (
     <QueueTask
       initial={{ x: -1000, opacity: 0 }}
       animate={{ x: 0, opacity: 1 }}
       exit={{ x: 1000, opacity: 0 }}
-      transition={{ duration: 0.2 * props.index < 1 ? 0.2 * props.index : 1 }}
+      transition={{ duration: 0.5, delay: index * 0.1, type: "spring" }}
       onClick={() => {
-        props.setSelectedTask(props.item);
-        props.onTaskClick();
+        setSelectedTask(item);
+        onTaskClick();
       }}
     >
-      <TaskTitle>{props.item.taskName}</TaskTitle>
-      <TaskDescription>{props.item.taskDescription}</TaskDescription>
+      <TaskLogo>
+        <FaBurn />
+      </TaskLogo>
+      <div>
+        <TaskTitle>{item.taskName}</TaskTitle>
+        <TaskDescription>{item.taskDescription}</TaskDescription>
+      </div>
     </QueueTask>
   );
 }
 export const QueueTask = styled(motion.div)`
+  display: flex;
+  /* flex-direction: column; */
   cursor: pointer;
   background-color: rgba(0, 0, 0, 0.1);
   /* backdrop-filter: blur(10px); */
@@ -29,13 +41,21 @@ export const QueueTask = styled(motion.div)`
 `;
 
 const TaskTitle = styled.h2`
-    font-weight: 700;
-`
+  font-weight: 700;
+`;
 const TaskDescription = styled.h2`
-    font-weight: 300;
-    color: #999;
-`
+  font-weight: 300;
+  color: #999;
+`;
 
+const TaskLogo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 2em;
+  width: 50px;
+  height: 50px;
+`;
 
 const slide = {
   initial: {
