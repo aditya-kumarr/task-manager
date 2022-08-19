4d9962133a4e6f6de18975f81a76a554114f19bd
aditya <adityakumar222333@gmail.com>
2022-08-19 12:37:52 +0530

    👌 IMPROVE: added a button and fixed height

--- src/containers/ListofText.jsx
+++ src/containers/ListofText.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+import { SButton } from "../components/Froms/ControlledFrom.style";
 
 // The main goal of this component is to show all the problems in a list  in small boxes
-export function ListofText({ title="Note", list=["this is a note","this is another note","this is also a note"] }) {
+export function ListofText({
+  title = "Note",
+  list = ["this is a note", "this is another note", "this is also a note"],
+  buttonPressHandler,
+}) {
   return (
-    <div>
+    <ListContainer>
       <h3>{title}</h3>
       {list.map((item, index) => {
         return (
@@ -12,6 +18,16 @@ export function ListofText({ title="Note", list=["this is a note","this is anoth
           </p>
         );
       })}
-    </div>
+      <SButton onClick={buttonPressHandler}>Add</SButton>
+    </ListContainer>
   );
 }
+
+const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-height: 250px;
+  padding-right: 0.5em;
+  overflow-y: scroll;
+  gap: 0.2em;
+`;
